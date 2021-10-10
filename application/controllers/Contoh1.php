9c0f6769de4108c1bd1abca23d90615a3ddea272
putridwi7 <dwiputriyuliana@gmail.com>
2021-10-10 17:53:32 +0700

    menambahkan pustaka booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Dwi Putri
+            Saya tinggal didaerah rawamangun
+            olahraga yang saya sukai adalah Basket";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dwi Putri</li>
+        <li>Nama Belakang : Yuliana</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Kincir VIII No.24,Pulogadung</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olahraga Favorite</h4>
+    <ul type="square">
+        <li>Basketball</li>
+        <li>Renang</li>
+    </ul>
+</section>
